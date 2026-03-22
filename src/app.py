b65876aefa665dbefbc96d6eab529748a438067b
Eweka01 <oseweka1@gmail.com>
2026-03-22 02:27:49 -0400

    Adds python-app with k8s deployments

---
+++ src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+	# Do an actual check here
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    import os
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host="0.0.0.0", port=port)
+
+
+
+
+
+#'/api/v1/details'
+#'api/v1/healthz'
