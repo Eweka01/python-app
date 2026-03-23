1b41f7b61c65a7ff76ff6ef195aaedf73fa3cacd
Eweka01 <oseweka1@gmail.com>
2026-03-23 14:41:32 -0400

    changed endpoints

--- src/app.py
+++ src/app.py
@@ -6,9 +6,9 @@ import socket
 app = Flask(__name__)
 
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
-def details():
+def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
